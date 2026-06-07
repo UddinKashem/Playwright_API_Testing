0e252cd165eeecccb5d8465050429fb08357be26
UddinKashem <30503392+UddinKashem@users.noreply.github.com>
2026-06-06 22:11:30 -0400

    Validate API Response

--- tests/api_test3.spec.js
+++ tests/api_test3.spec.js
@@ -4,11 +4,15 @@ test.describe('API Testing with Playwright', () => {
 
   test('GET: Validate list of users', async ({ request }) => {
     const response = await request.get('https://reqres.in/api/users?page=2');
-
-    expect(response.ok()).toBeTruthy();
+      //https://reqres.in/api/users?page=2');
 
     const body = await response.json();
     console.log(body);
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+
+    // const body = await response.json();
+    // console.log(body);
 
     expect(body.page).toBe(2);
     expect(body.data.length).toBeGreaterThan(0);
